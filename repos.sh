c075523620a69b3660f27abba2e2b79145d52863
GNOME SDK Maintenance User <gnomesdk@spinner.gnome.org>
2016-08-24 17:14:22 +0000

    Add support for nightly builds

--- repos.sh
+++ repos.sh
@@ -1,44 +1,25 @@
 #!/bin/sh
 
-staging_repo=/srv/gnome-sdk/public_html/staging/repo
+stable_staging_repo=/srv/gnome-sdk/public_html/staging/repo
+stable_remotes="sdkbuilder1 aarch64-stable arm-stable"
+
+nightly_staging_repo=/srv/gnome-sdk/public_html/staging/repo-nightly
+nightly_remotes="sdkbuilder1 aarch64-unstable arm-unstable"
+
 stable_runtime_repo=/srv/gnome-sdk/public_html/repo
 stable_app_repo=/srv/gnome-sdk/public_html/repo-apps
-stable_remotes="sdkbuilder1 aarch64-stable arm-stable"
-runtime_regexp="^runtime/\(org.gnome.\(Sdk\|Platform\)\|org.freedesktop.\(Base\)\?\(Sdk\|Platform\)\)"
+
+nightly_runtime_repo=/srv/gnome-sdk/public_html/nightly/repo
+nightly_app_repo=/srv/gnome-sdk/public_html/nightly/repo-apps
+
 stable_gpg_args="--gpg-homedir=/srv/gnome-sdk/gnupg --gpg-sign=55D15281"
+nightly_gpg_args="--gpg-homedir=/srv/gnome-sdk/gnupg-nightly --gpg-sign=82170E3D"
+
+runtime_regexp="^runtime/\(org.gnome.\(Sdk\|Platform\)\|org.freedesktop.\(Base\)\?\(Sdk\|Platform\)\)"
 
 set -e
 set -u
-
-function pullStableRemote() {
-    local remote=$1
-    ostree --repo=${staging_repo} pull --mirror ${remote}
-}
-
-function pullStableAll() {
-    for r in ${stable_remotes}; do
-	pullStableRemote ${r}
-    done
-}
-
-function listStableRefs() {
-    ostree --repo=${staging_repo} refs | grep -v ^appstream/ | LC_COLLATE=C sort
-}
-
-function listStableRuntimeRefs() {
-    listStableRefs | grep ${runtime_regexp}
-}
-
-function listStableAppRefs() {
-    listStableRefs | grep -v ${runtime_regexp}
-}
-
-function getSummary() {
-    local repo=$1
-    local ref=$2
-
-    ostree show --repo=${repo} ${ref} | head -4 | tail -1 | sed "s/^    //"
-}
+set -x
 
 function mergeRefs() {
     local destrepo=$1
@@ -49,11 +30,62 @@ function mergeRefs() {
     flatpak build-commit-from --no-update-summary --src-repo=${srcrepo} ${gpg_args-} ${destrepo} ${refs-}
 }
 
+function pullStableRemote() {
+    local remote=$1
+    ostree --repo=${stable_staging_repo} pull --mirror ${remote}
+}
+
+function pullStableAll() {
+    for r in ${stable_remotes}; do
+	pullStableRemote ${r}
+    done
+}
+
+function pullNightlyRemote() {
+    local remote=$1
+    ostree --repo=${nightly_staging_repo} pull --mirror ${remote}
+}
+
+function pullNightlyAll() {
+    for r in ${nightly_remotes}; do
+	pullNightlyRemote ${r}
+    done
+}
+
+function listStableRefs() {
+    ostree --repo=${stable_staging_repo} refs | grep -v ^appstream/ | LC_COLLATE=C sort
+}
+
+function listStableRuntimeRefs() {
+    listStableRefs | grep ${runtime_regexp}
+}
+
+function listStableAppRefs() {
+    listStableRefs | grep -v ${runtime_regexp}
+}
+
+function listNightlyRefs() {
+    ostree --repo=${nightly_staging_repo} refs | grep -v ^appstream/ | LC_COLLATE=C sort
+}
+
+function listNightlyRuntimeRefs() {
+    listNightlyRefs | grep ${runtime_regexp}
+}
+
+function listNightlyAppRefs() {
+    listNightlyRefs | grep -v ${runtime_regexp}
+}
+
 arg_pull_stable=false
+arg_pull_nightly=false
 arg_update_stable_apps=false
 arg_update_stable_runtimes=false
+arg_update_nightly_apps=false
+arg_update_nightly_runtimes=false
 arg_merge_stable_apps=false
 arg_merge_stable_runtimes=false
+arg_merge_nightly_apps=false
+arg_merge_nightly_runtimes=false
 arg_disable_deltas=false
 
 function usage () {
@@ -64,9 +96,13 @@ function usage () {
     echo
     echo "  -h --help                      Display this help message and exit"
     echo "  --pull-stable                  Pull stable remotes into staging repo"
+    echo "  --pull-nightly                 Pull nightly remotes into staging repo"
     echo "  --merge-stable                 Merge stable apps + runtimes"
     echo "  --merge-stable-apps            Merge stable apps"
     echo "  --merge-stable-runtimes        Merge stable runtimes"
+    echo "  --merge-nightly                Merge nightly apps + runtimes"
+    echo "  --merge-nightly-apps           Merge nightly apps"
+    echo "  --merge-nightly-runtimes       Merge nightly runtimes"
     echo "  --disable-deltas               Don't create static deltas"
     echo
 }
@@ -83,6 +119,10 @@ while : ; do
 	    arg_pull_stable=true
 	    shift ;;
 
+	--pull-nightly)
+	    arg_pull_nightly=true
+	    shift ;;
+
 	--merge-stable)
 	    arg_merge_stable_apps=true
 	    arg_merge_stable_runtimes=true
@@ -94,6 +134,19 @@ while : ; do
 
 	--merge-stable-runtimes)
 	    arg_merge_stable_runtimes=true
+	    shift ;;
+
+	--merge-nightly)
+	    arg_merge_nightly_apps=true
+	    arg_merge_nightly_runtimes=true
+	    shift ;;
+
+	--merge-nightly-apps)
+	    arg_merge_nightly_apps=true
+	    shift ;;
+
+	--merge-nightly-runtimes)
+	    arg_merge_nightly_runtimes=true
 	    shift ;;
 
 	--disable-deltas)
@@ -109,14 +162,8 @@ if $arg_pull_stable; then
     pullStableAll
 fi
 
-if $arg_merge_stable_runtimes; then
-    mergeRefs ${stable_runtime_repo} ${staging_repo} "$(listStableRuntimeRefs)" "${stable_gpg_args}"
-    arg_update_stable_runtimes=true
-fi
-
-if $arg_merge_stable_apps; then
-    mergeRefs ${stable_app_repo} ${staging_repo} "$(listStableAppRefs)" "${stable_gpg_args}"
-    arg_update_stable_apps=true
+if $arg_pull_nightly; then
+    pullNightlyAll
 fi
 
 delta_args=--generate-static-deltas
@@ -124,10 +171,38 @@ if $arg_disable_deltas; then
     delta_args=
 fi
 
+if $arg_merge_stable_runtimes; then
+    mergeRefs ${stable_runtime_repo} ${stable_staging_repo} "$(listStableRuntimeRefs)" "${stable_gpg_args}"
+    arg_update_stable_runtimes=true
+fi
+
 if $arg_update_stable_runtimes; then
     flatpak build-update-repo ${delta_args} ${stable_gpg_args} ${stable_runtime_repo}
 fi
 
+if $arg_merge_stable_apps; then
+    mergeRefs ${stable_app_repo} ${stable_staging_repo} "$(listStableAppRefs)" "${stable_gpg_args}"
+    arg_update_stable_apps=true
+fi
+
 if $arg_update_stable_apps; then
     flatpak build-update-repo  ${delta_args} ${stable_gpg_args} ${stable_app_repo}
+fi
+
+if $arg_merge_nightly_runtimes; then
+    mergeRefs ${nightly_runtime_repo} ${nightly_staging_repo} "$(listNightlyRuntimeRefs)" "${nightly_gpg_args}"
+    arg_update_nightly_runtimes=true
+fi
+
+if $arg_update_nightly_runtimes; then
+    flatpak build-update-repo ${delta_args} ${nightly_gpg_args} ${nightly_runtime_repo}
+fi
+
+if $arg_merge_nightly_apps; then
+    mergeRefs ${nightly_app_repo} ${nightly_staging_repo} "$(listNightlyAppRefs)" "${nightly_gpg_args}"
+    arg_update_nightly_apps=true
+fi
+
+if $arg_update_nightly_apps; then
+    flatpak build-update-repo  ${delta_args} ${nightly_gpg_args} ${nightly_app_repo}
 fi
